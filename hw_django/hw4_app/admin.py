48e8d966e102d7149f5ab5c9e5280b766d0f1fcc
Vladimir Pyat <scared@mail.ru>
2024-04-19 10:35:25 +0300

    HW05_v0 add admin

--- hw_django/hw4_app/admin.py
+++ hw_django/hw4_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Client, Order, Product
 
-# Register your models here.
+@admin.action(description="Установить количество=1")
+def drop_quantity(modeladmin, request, queryset):
+    queryset.update(qty=1)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["pk", "name", "description", "price", "qty"]
+    actions = [drop_quantity]
+
+
+admin.site.register(Client)
+admin.site.register(Order)
+admin.site.register(Product, ProductAdmin)
